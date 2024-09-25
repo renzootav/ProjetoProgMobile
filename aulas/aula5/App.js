91d261af4e7093c20b2c97d5562d80489577af4a
Gabriel Lunguinho <gabriel.lunguinho1707@gmail.com>
2024-09-25 15:33:04 -0300

    Código do material

--- aulas/aula5/App.js
+++ aulas/aula5/App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, View, VirtualizedList, StyleSheet, Text, StatusBar,} from 'react-native';
 
-export default function App() {
+const getItem = (_data, index) => ({
+  id: Math.random().toString(12).substring(0),
+  title: `Item ${index + 1}`,
+});
+
+const getItemCount = (_data) => 50;
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <VirtualizedList
+        initialNumToRender={4}
+        renderItem={({ item }) => <Item title={item.title} />}
+        keyExtractor={(item) => item.id}
+        getItemCount={getItemCount}
+        getItem={getItem}
+      />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    height: 150,
     justifyContent: 'center',
+    marginVertical: 8,
+    marginHorizontal: 16,
+    padding: 20,
+  },
+  title: {
+    fontSize: 32,
   },
 });
+
+export default App;
