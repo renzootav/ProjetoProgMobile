f52b762a4855b44c3418311eda4dee04b7784ce2
Gabriel Lunguinho <gabriel.lunguinho1707@gmail.com>
2024-09-25 15:19:42 -0300

    Código do material

--- aulas/aula4/App.js
+++ aulas/aula4/App.js
@@ -1,10 +1,17 @@
+// Juntando tudo e testando
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Aplicativo de Login!</Text>
+      <TextInput style={styles.input} keyboardType='numeric' />
+      <Button title='Login' onPress={() => alert('Ok')} />
+      <Text style={styles.esquecisenha}>Esqueci minha senha</Text>
+      <Button title='Entrar com Facebook' onPress={() => alert('Entrando com facebook')} />
+      <Button title='Entrar com Google' onPress={() => alert('Entrando com google')} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +23,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    width: 200,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  esquecisenha: {
+    textDecorationLine: 'underline',
   },
 });
