b54fd3eb29f9ca08d38fbf548a09d76a27a1a3ea
Gabriel Lunguinho <gabriel.lunguinho1707@gmail.com>
2024-09-25 22:48:16 -0300

    Código do material

---
+++ aulas/aula6/screens/about.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+function AboutScreen({ navigation }) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#f0f',
+      }}>
+      <Text>About Screen</Text>
+      <Button
+        title="Ir para Home"
+        onPress={() => navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+export default AboutScreen;
